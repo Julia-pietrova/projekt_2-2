25792de94a4cc096dc04daca89d5882ebec62a76
Your Name <you@example.com>
2024-06-07 09:41:55 +0200

    protection for entering characters below 0

--- index.js
+++ index.js
@@ -9,16 +9,25 @@ document.getElementById("convert").addEventListener("click", () => {
     return;
   }
 
+  if (amount <= 0) {
+    alert("Wprowadź kwotę większą niż 0.");
+    return;
+  }
+
   loader.style.display = "block";
   result.innerHTML = "";
 
   fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${currency}/?format=json`)
     .then((response) => response.json())
     .then((data) => {
-      const rate = data.rates[0].mid;
-      const convertedAmount = (amount * rate).toFixed(2);
-      result.innerHTML = `to ${convertedAmount} PLN`;
-    })
+      const rate = data?.rates?.[0]?.mid;
+      if(rate) {
+        const convertedAmount = (amount * rate).toFixed(2);
+        result.innerHTML = `to ${convertedAmount} PLN`;
+      } else {
+        alert("Nie udało się pobrać kursów wymiany.");
+      }
+})
     .catch((error) => {
       console.error("Błąd podczas pobierania kursów wymiany:", error);
       alert("Nie udało się pobrać kursów wymiany. Spróbuj ponownie później.");
